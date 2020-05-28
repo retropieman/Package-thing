1ad06f9d295671fdc422819a782d793e700a4c69
Witherman17 <31626399+retropieman@users.noreply.github.com>
2020-05-28 11:59:26 -0600

    Create test.js

---
+++ test.js
@@ -0,0 +1,9 @@
+var cr = require('./index');
+
+cr.stinkit("stinkit", "Appended by stinkit!");
+
+var crap = cr.blink([1, 2]);
+console.log(crap);
+
+var crap = cr.blargh(12, 23, 24, 324);
+console.log(crap);
